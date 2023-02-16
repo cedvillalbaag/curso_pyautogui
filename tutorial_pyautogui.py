d8d246305edc08d7e12823335031332f65da800d
cedvillalbaag <ced.villalbaag@gmail.com>
2023-02-16 09:28:36 -0500

    actualizar

--- tutorial_pyautogui.py
+++ tutorial_pyautogui.py
@@ -5,7 +5,8 @@
 
 #Importar librería
 import pyautogui
-
+import time
+import opencv
 
 #Resolucion de pantalla
 #print(pyautogui.size())
@@ -74,3 +75,63 @@ pyautogui.moveRel(100,0, 1)
 pyautogui.click()
 
 pyautogui.write("Hola , esto es una prueba de lo que se puede hacer en programacion", interval = 0.05)
+
+############################################################
+
+#UBICAR EN LA PANTALLA
+
+#Localizar la imaen en pantalla
+res = pyautogui.locateOnScreen("image.png")
+
+#Indicar las coordenadas del centro de la imagen
+editbutton = pyautogui.center(res)
+
+#Mover el mouse al centro de la imagen
+pyautogui.moveTo(editbutton)
+
+
+#Localizar el centro de la imagen en pantalla
+pyautogui.locateCenterOnScreen("image.png")
+print(res)
+
+
+
+#Ejercicio Busqueda en YT
+###################################################
+# 1.- Abrir nueva pestaña en navegador
+# 2.- Buscar youtube.com
+# 3.- Buscar el canal al cual quieres suscribirte
+# 4.- Click en el canal
+# 5.- Click en el boton de suscripcion
+
+
+#Indicar el nombre del canal que queremos buscar
+channel_name = prompt("Enter the channel name:")
+print(channel_name)
+
+
+#Abrir nueva pestaña
+pyautogui.hotkey('ctrl','t')
+time.sleep(1)
+
+#Buscar Youtube
+pyautogui.write("https://www.youtube.com/")
+pyautogui.hotkey('enter')
+time.sleep(1)
+
+#Buscar el caja de busqueda (Search Box)
+x,y = pyautogui.locateCenterOnScreen("image.png", confidence = 0.8)
+pyautogui.moveTo(search_button,1)
+pyautogui.click()
+pyautogui.write(channel_name)
+pyautogui.hotkey('enter')
+
+#Buscar el logo del canal (logo)
+x,y = pyautogui.locateCenterOnScreen("logo.png", confidence = 0.8)
+pyautogui.moveTo(x,y,1)
+pyautogui.click()
+
+#Buscar el boton de suscripcion
+x,y = pyautogui.locateCenterOnScreen("boton_suscripcion.png", confidence = 0.8)
+pyautogui.moveTo(x,y, 1)
+pyautogui.click()
